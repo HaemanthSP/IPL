d3dcce2c108c7f17c41c11a4a91c43ff3fb35661
haemanthsp <haemanthsp@gmail.com>
2023-02-17 16:30:51 +0100

    End of 2022

--- gameplan.py
+++ gameplan.py
@@ -9,13 +9,13 @@ class Team:
         self.rank = rank
         self.fg_color = code
         self.matches = []
-        self.weekbg = {'MON': 229,
-                       'TUE': 230,
-                       'WED': 157,
-                       'THU': 193,
-                       'FRI': 195,
-                       'SAT': 223,
-                       'SUN': 225}
+        self.weekbg = {'Mon': 229,
+                       'Tue': 230,
+                       'Wed': 157,
+                       'Thu': 193,
+                       'Fri': 195,
+                       'Sat': 223,
+                       'Sun': 225}
 
     def add_match(self, match):
         # print('Adding match %s to Team %s' % (match.id, self.name))
@@ -34,14 +34,14 @@ class Team:
                 bg_color = self.weekbg[matches[i].day]
                 res.append('%s%s*%s' % (fg(self.fg_color), bg(bg_color), attr(0)))
             else:
-                res.append('%s=%s' % (fg(self.fg_color), attr(0))) 
+                res.append('%s=%s' % (fg(self.fg_color), attr(0)))
 
         # Focus with in the range
         res = res[start-1:]
-        
+
         print("%s%s%s\t\t%s" % (fg(self.fg_color), self.abbr, attr(0), ''.join(res)))
-            
-    
+
+
     def heat_map_wo_bg(self, start, max_matches):
         matches = {match.id:match for match in self.matches}
         res = ['%s%s%s' % (fg(255 if i in matches else self.fg_color),
@@ -52,14 +52,14 @@ class Team:
         # Focus with in the range
         res = res[start-1:]
         return res
-       
+
 
 class Venue:
     def __init__(self, name:str, sym:str):
         self.name = name
         self.sym = sym
-        self.matches = [] 
-    
+        self.matches = []
+
     def add_match(self, match:any):
         # print('Adding match %s to the Venue %s' % (match.id, self.name))
         self.matches.append(match)
@@ -76,7 +76,7 @@ class Match:
         self.id = match_id
         self.teams = [teamA, teamB]
         self.venue = venue
-        self.date = date 
+        self.date = date
         self.day = day
         self.update()
 
@@ -95,7 +95,7 @@ class Gameplan:
         self.load_teams(teams_file)
         self.load_venues(venue_file)
         self.load_matches(fixtures)
-        
+
     def load_teams(self, teams_file):
         self.teams = dict()
         with open(teams_file, 'r') as tf:
@@ -103,6 +103,7 @@ class Gameplan:
             for line in lines:
                 name, abbr, code = line.strip().split('\t')
                 self.teams[abbr] = Team(name, abbr, code)
+        # print(self.teams)
 
     def load_venues(self, venue_file):
         self.venues = dict()
@@ -111,13 +112,13 @@ class Gameplan:
             for line in lines:
                 name, sym = line.strip().split('\t')
                 self.venues[name] = Venue(name, sym)
-    
+
     def load_matches(self, fixtures):
         self.matches = list()
         with open(fixtures, 'r') as ff:
             lines = ff.readlines()
             for line in lines:
-                match_id, day, date, venue, teamA, teamB = line.strip().split('\t')
+                match_id, day, date, teamA, teamB, venue = line.strip().split('\t')
                 self.matches.append(Match(int(match_id),
                                           self.teams[teamA],
                                           self.teams[teamB],
@@ -132,13 +133,13 @@ class Gameplan:
 
     def day_map(self, start):
         cmap = {
-                'MON': '%sM%s'%(fg(229), attr(0)),
-                'TUE': '%sT%s'%(fg(230), attr(0)),
-                'WED': '%sW%s'%(fg(157), attr(0)),
-                'THU': '%st%s'%(fg(193), attr(0)),
-                'FRI': '%sF%s'%(fg(195), attr(0)),
-                'SAT': '%sS%s'%(fg(223), attr(0)),
-                'SUN': '%s$%s'%(fg(225), attr(0))
+                'Mon': '%sM%s'%(fg(229), attr(0)),
+                'Tue': '%sT%s'%(fg(230), attr(0)),
+                'Wed': '%sW%s'%(fg(157), attr(0)),
+                'Thu': '%st%s'%(fg(193), attr(0)),
+                'Fri': '%sF%s'%(fg(195), attr(0)),
+                'Sat': '%sS%s'%(fg(223), attr(0)),
+                'Sun': '%s$%s'%(fg(225), attr(0))
                 }
         sorted_list = sorted(self.matches, key=lambda x: x.id)
         venue = [cmap[x.day] for x in sorted_list][start-1:]
@@ -153,7 +154,7 @@ class Gameplan:
         venue = [x.venue.sym for x in sorted_list][start-1:]
         print("\t\t%s" % (''.join(venue)))
 
-    def heat_maps(self, start=1, max_matches=60):
+    def heat_maps(self, start=1, max_matches=80):
         teams = sorted(self.teams.items(), key=lambda x: x[1].rank)
         self.day_map(start)
         map_list = []
@@ -161,11 +162,11 @@ class Gameplan:
             res = team.heat_map_wo_bg(start, max_matches)
             density = self.match_density(res)
             map_list.append((density[0] , "%s%s%s\t\t%s" % (fg(team.fg_color), team.abbr, attr(0), ''.join(res))))
-        
+
         print("\n".join([y for _, y in sorted(map_list, key=lambda t: -t[0])]))
         self.venue_map(start)
 
-        print("\n".join([str(x) for x, _ in sorted(map_list, key=lambda t: -t[0])]))
+        # print("\n".join([str(x) for x, _ in sorted(map_list, key=lambda t: -t[0])]))
 
     def match_density(self, fixture):
         last_match = None
@@ -182,8 +183,8 @@ class Gameplan:
                 if (idx - last_match) > max_offset:
                     count = 0
                 update = max(0, count - 0.5)
-            density.append(update) 
-                
+            density.append(update)
+
         return density[::-1]
 
 
